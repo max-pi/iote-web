46031b32c0c85c379a4e45f28278094e5c99524f
Willie <cheongwillie@gmail.com>
2016-01-27 17:31:09 -0500

    opening up endpoints for user resource

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,11 +6,35 @@ use Iote\Models\UserModel;
 
 class UserController extends BaseController {
 
-	public function getIndex() {
+	/*********************************
+	 * Returns active user if no query params are specified
+	 * 	Allowable query params are `id`, `email`, `phone` */
+	public function getIndex() { // AUTHENTICATION REQUIRED
 		return $this->makeSuccess(
 			"OK",
-			UserModel::where('emails', 'cheongwillie@gmail.com')->first()
+			UserModel::find("56a819f8744d8446113393e7")
+			//UserModel::where('emails', 'cheongwillie@gmail.com')->first()
 		);
 	}
 
+	/*********************************
+	 * Starts process for adding contact to current user
+	 * 	Required params are [`email` OR `phone`] */
+	public function postContact() { // AUTHENTICATION REQUIRED
+		return $this->makeSuccess("sending verification messages");
+	}
+
+	/*********************************
+	 * Creates new user object and sends verification messages
+	 * 	Required params are [`email` OR `phone`] and `password` */
+	public function postRegister() { // AUTHENTICATION OPTIONAL
+		return $this->makeSuccess("registering");
+	}
+
+	/*********************************
+	 * Confirms an email or phone record for a user
+	 * 	Required params are [`email` OR `phone`] and `confirmationKey` */
+	public function postConfirm() { // AUTHENTICATION OPTIONAL
+		return $this->makeSuccess("confirming user");
+	}
 }
--- timelog.md
+++ timelog.md
@@ -1,4 +1,5 @@
-| Date        | Time spent | Description |
-|-------------|------------|-------------|
-| 26 Jan 2016 | 6 hrs      | Research and starting port over to Laravel |
-| 27 Jan 2016 | 2 hrs      | More clean up on fresh Laravel installation |
+| Date | Hours | Description |
+|------|-------|-------------|
+| 26 Jan 2016 | 6 | Research and starting port over to Laravel |
+| 27 Jan 2016 | 2 | More clean up on fresh Laravel installation |
+| 27 Jan 2016 | 1 | Getting started on application level code |
