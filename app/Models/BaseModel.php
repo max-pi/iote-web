d3ea9297e8debcb771fd977c05f1ff71375e7263
Willie <cheongwillie@gmail.com>
2016-02-01 00:08:14 -0500

    taking out schema builder, enabling soft deletes

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -3,16 +3,11 @@
 namespace Iote\Models;
 
 use Jenssegers\Mongodb\Model as Moloquent;
-use Illuminate\Database\Schema\Builder;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
 
 class BaseModel extends Moloquent {
 	public $incrementing = false;
 
-	public function __construct($attributes = array()) {
-		parent::__construct($attributes);
-
-		if (Schema::hasCollection($this->collection)) {
-			Schema::create($this->collection, function($c) { });
-		}
-	}
+	use SoftDeletes;
+	protected $dates = ['deleted_at'];
 }
